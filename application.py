0b08c198e59d0522cdddc30b7435a965d0808623
Miguel Gonzalez <migueldeperosanz@yahoo.es>
2022-03-17 01:29:47 +0100

    application modified into loop

--- application.py
+++ application.py
@@ -28,9 +28,11 @@ def api():
     home_team = df[df['Name']==home_team_menu]
     away_team = df[df['Name']==away_team_menu]
     
-    X1 = np.array(home_team[['OVA', 'ATT', 'MID', 'DEF', 'Transfer Budget(M€)', 'Speed', 'Dribbling', 'Passing', 'Positioning', 'Crossing','Passing.1', 'Shooting', 'Positioning.1', 'Aggression', 'Pressure', 'Team Width', 'Defender Line', 'DP', 'IP']])
+    X1 = np.array(home_team[['OVA', 'ATT', 'MID', 'DEF', 'Transfer Budget(M€)', 'Speed', 'Dribbling', 'Passing', 'Positioning', 'Crossing','Passing.1', 
+                             'Shooting', 'Positioning.1', 'Aggression', 'Pressure', 'Team Width', 'Defender Line', 'DP', 'IP']])
         
-    X2 = np.array(away_team[['OVA', 'ATT', 'MID', 'DEF', 'Transfer Budget(M€)', 'Speed', 'Dribbling', 'Passing', 'Positioning', 'Crossing','Passing.1', 'Shooting', 'Positioning.1', 'Aggression', 'Pressure', 'Team Width', 'Defender Line', 'DP', 'IP']])
+    X2 = np.array(away_team[['OVA', 'ATT', 'MID', 'DEF', 'Transfer Budget(M€)', 'Speed', 'Dribbling', 'Passing', 'Positioning', 'Crossing','Passing.1', 
+                             'Shooting', 'Positioning.1', 'Aggression', 'Pressure', 'Team Width', 'Defender Line', 'DP', 'IP']])
         
     X = np.concatenate((X1, X2), axis=None).astype(int)
     X = X.reshape(1, -1)
@@ -38,83 +40,49 @@ def api():
     prediction = model_logreg.predict(X)     #MATCH WINNER
     prediction2 = model_logreg.predict_proba(X)     #PROBABILITIES OF WINNING THE MATCH
     prediction3 = model_logreg_half.predict_proba(X)     #PROBABILITIES OF WINNING ON THE HALF TIME
-    prediction_24 = model_linreg_24.predict(X) 
+    prediction_24 = model_linreg_24.predict(X) # LINEAR REGRESSION FOR THE REST OF VARIABLES
   
-    prob_away_win_half = round(100*prediction3[0,0],1)     #PROBABILITY AWAY TEAM WINS
-    prob_draw_game_half = round(100*prediction3[0,1],1)    #PROBABILITY DRAW GAME
-    prob_home_win_half = round(100*prediction3[0,2],1)    #PROBABILITY HOME TEAM WINS
-       
-    prob_away_win = round(100*prediction2[0,0],1)     #PROBABILITY AWAY TEAM WINS
-    prob_draw_game = round(100*prediction2[0,1],1)    #PROBABILITY DRAW GAME
-    prob_home_win = round(100*prediction2[0,2],1)    #PROBABILITY HOME TEAM WINS
-     
-    total_goals_final = round(prediction_24[0,0],2)
-    home_goals_final = round(prediction_24[0,1],2)
-    away_goals_final = round(prediction_24[0,2],2)
     
-    total_goals_half = round(prediction_24[0,3],2)
-    home_goals_half = round(prediction_24[0,4],2)
-    away_goals_half = round(prediction_24[0,5],2)
-
+    data1=[]    
+    data2=[] 
+    list1=['prob_away_win_half','prob_draw_game_half','prob_home_win_half']    
+    list2=['prob_away_win','prob_draw_game','prob_home_win']
     
-    home_team_shots = round(prediction_24[0,6],1)
-    away_team_shots = round(prediction_24[0,7],1)
-    full_time_shots = round(prediction_24[0,8],1)
-
     
-    home_team_shots_target = round(prediction_24[0,9],1)
-    away_team_shots_target = round(prediction_24[0,10],1)
-    full_time_shots_target = round(prediction_24[0,11],1)
+    for i in range (0,3):
+        x=round(100*prediction3[0,i],2)
+        y=round(100*prediction2[0,i],2)
+        data1.append(x)
+        data2.append(y)
+        
+    dictionary1 = dict(zip(list1, data1))
+    dictionary2 = dict(zip(list2, data2))
+   
+    data3=[] 
+    list3=['total_goals_final','home_goals_final','away_goals_final','total_goals_half','home_goals_half','away_goals_half',
+          'home_team_shots','away_team_shots','full_time_shots','home_team_shots_target','away_team_shots_target','full_time_shots_target',
+          'home_team_corners','away_team_corners','full_time_corners','home_team_fouls_comitted','away_team_fouls_comitted',
+          'Total_fouls_comitted','home_team_yellow_cards','away_team_yellow_cards','Total_yellow_cards','home_team_red_cards',
+          'away_team_red_cards','Total_red_cards']  
+   
+    for i in range (0,24):
+        x=round(prediction_24[0,i],2)
+        data3.append(x)
 
+    dictionary3 = dict(zip(list3, data3))
+    total_dictionary = dict(dictionary1, **dictionary2, **dictionary3)
     
-    home_team_corners = round(prediction_24[0,12],1)
-    away_team_corners = round(prediction_24[0,13],1)
-    full_time_corners = round(prediction_24[0,14],1)
-
+    return jsonify(total_dictionary)
     
-    home_team_fouls_comitted = round(prediction_24[0,15],1)
-    away_team_fouls_comitted = round(prediction_24[0,16],1)
-    Total_fouls_comitted = round(prediction_24[0,17],1)
-
     
-    home_team_yellow_cards = round(prediction_24[0,18],1)
-    away_team_yellow_cards = round(prediction_24[0,19],1)
-    Total_yellow_cards = round(prediction_24[0,20],1)
+@application.route('/predict')
 
-    
-    home_team_red_cards = round(prediction_24[0,21],1)
-    away_team_red_cards = round(prediction_24[0,22],1)
-    Total_red_cards = round(prediction_24[0,23],1)
-
- 
-    return jsonify({'Probability home win half time (%)':str(prob_home_win_half),
-                    'Probability draw game half time (%)':str(prob_draw_game_half),
-                    'Probability away win half time (%)':str(prob_away_win_half),
-                    'Half time total goals':str(total_goals_half),
-                    'Half time home goals':str(home_goals_half),
-                    'Half time away goals':str(away_goals_half),
-                    'Full time total goals':str(total_goals_final),
-                    'Full time home goals':str(home_goals_final),
-                    'Full time away goals':str(away_goals_final),
-                    'Full time shots':str(full_time_shots),
-                    'Home team shots':str(home_team_shots),
-                    'Away team shots':str(away_team_shots),
-                    'Full time shots on target':str(full_time_shots_target),
-                    'Home team shots on target':str(home_team_shots_target),
-                    'Away team shots on target':str(away_team_shots_target),
-                    'Full time corners':str(full_time_corners),
-                    'Home team corners':str(home_team_corners),
-                    'Away team corners':str(away_team_corners),
-                    'Full time fouls comitted':str(Total_fouls_comitted),
-                    'Home team fouls comitted':str(home_team_fouls_comitted),
-                    'Away team fouls comitted':str(away_team_fouls_comitted),
-                    'Full time yellow cards':str(Total_yellow_cards),
-                    'Home team yellow cards':str(home_team_yellow_cards),
-                    'Away team yellow cards':str(away_team_yellow_cards),
-                    'Full time red cards':str(Total_red_cards),
-                    'Home team red cards':str(home_team_red_cards),
-                    'Away team red cards':str(away_team_red_cards)})
+def predict():
+    return('hello')
 
 
 if __name__ == "__main__":
     application.run(debug=True)
+    
+    
+
